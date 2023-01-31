99773aeba9d60a5be69d94d4b5e771150fbe5963
Nathan Culley <nathan@nathanculley.com>
2023-01-30 20:18:10 -0500

    Delete extraneous game functions and rewrite event listeners for player buttons
    Rewrote roughly half of all the JavaScript for the game to run more efficiently and reflect the new display system.

--- scripts.js
+++ scripts.js
@@ -2,85 +2,56 @@
 let playerScore = 0;
 let computerScore = 0;
 
-//links HTML buttons to script and adds an event listener that executes the basic functions of the game.
-//Checks the victory condition and terminates the process if the condition is met (checkVictory()).
-//Initiates the playing of one round, assigning the result to roundResult (startRound()).
-//Adds one point to the score of the round winner, if there was one (addScore()).
-//Displays the results of the round, including moves and scores (displayRound()).
-//Checks the victory condition again; terminates the game and displays the game result if the condition is met (endGame()).
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const gameMessage = document.getElementById('game-message');
+
+playerScoreDisplay.textContent = playerScore;
+computerScoreDisplay.textContent = computerScore;
+gameMessage.textContent = "Game on!";
+
+
 const rockButton = document.getElementById('rockButton');
 const paperButton = document.getElementById('paperButton');
 const scissorsButton = document.getElementById('scissorsButton');
 
 rockButton.addEventListener('click', function(event) {
-    if (checkVictory(playerScore, computerScore)) {
-        alert('Game Over. Refresh page to restart.');
-        return;
+    if (playRound("rock", computerPlay()) == 'playerWin') {
+        playerScore = addScore(playerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay();
     }
-    let roundResult = startRound('rock', playerScore, computerScore);
-    console.log(roundResult);
-    if (roundResult == 'playerWin') {
-        playerScore = addScore(playerScore)
-    }
-    if (roundResult == 'computerWin') {
+    if (playRound("rock", computerPlay()) == 'computerWin') {
         computerScore = addScore(computerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay();
     }
-    displayRound('rock',roundResult, playerScore, computerScore);
-    if (checkVictory(playerScore, computerScore)) {
-        console.log('Game over!');
-        endGame(playerScore, computerScore);
-    }
-    console.log(checkVictory(),playerScore,computerScore);
 });
 paperButton.addEventListener('click', function(event) {
-    if (checkVictory(playerScore, computerScore)) {
-        alert('Game Over. Refresh page to restart.');
-        return;
+    if (playRound("paper", computerPlay()) == 'playerWin') {
+        playerScore = addScore(playerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay();
     }
-    let roundResult = startRound('paper', playerScore, computerScore);
-    console.log(roundResult);
-    if (roundResult == 'playerWin') {
-        playerScore = addScore(playerScore)
-    }
-    if (roundResult == 'computerWin') {
+    if (playRound("paper", computerPlay()) == 'computerWin') {
         computerScore = addScore(computerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay();
     }
-    displayRound('paper',roundResult, playerScore, computerScore);
-    if (checkVictory(playerScore, computerScore)) {
-        console.log('Game over!');
-        endGame(playerScore, computerScore);
-    }
-    console.log(checkVictory(),playerScore,computerScore);
 });
 scissorsButton.addEventListener('click', function(event) {
-    if (checkVictory(playerScore, computerScore)) {
-        alert('Game Over. Refresh page to restart.');
-        return;
+    if (playRound("scissors", computerPlay()) == 'playerWin') {
+        playerScore = addScore(playerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay()
     }
-    let roundResult = startRound('scissors', playerScore, computerScore);
-    console.log(roundResult);
-    if (roundResult == 'playerWin') {
-        playerScore = addScore(playerScore)
-    }
-    if (roundResult == 'computerWin') {
+    if (playRound("scissors", computerPlay()) == 'computerWin') {
         computerScore = addScore(computerScore);
+        console.log(playerScore, computerScore);
+        updateDisplay();
     }
-    displayRound('scissors',roundResult, playerScore, computerScore);
-    if (checkVictory(playerScore, computerScore)) {
-        console.log('Game over!');
-        endGame(playerScore, computerScore);
-    }
-    console.log(checkVictory(),playerScore,computerScore);
 });
 
-//Determines the computer's move (computerPlay());
-//Compares the player's move and the computer's move and determines a winner (playRound()).
-//returns the result to roundWinner, which is returned to the event listener.
-function startRound(playerSelection, playerScore, computerScore) {
-    computerSelection = computerPlay();
-    let roundWinner = playRound(playerSelection, computerSelection);
-    return roundWinner;
-}
 
 //Randomly selects one of the three possible moves.
 function computerPlay() {
@@ -128,82 +99,15 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-//Creates a new list item in the element id="roundList" and uses the item to display the moves, the result, and the current score.
-function displayRound(playerMove,roundResult, playerScore, computerScore) {
-    let displayWinner;
-    let displayScore;
-    let computerMove;
-    const roundList = document.getElementById('roundList');
-    const li = document.createElement('li');
-    if (playerMove == 'rock') {
-        if (roundResult == 'playerWin') {
-            computerMove = 'scissors';
-            displayWinner = 'You won the round!';
-        }
-        if (roundResult == 'computerWin') {
-            computerMove = 'paper'
-            displayWinner = 'The computer won the round!';
-        }
-        if (roundResult == 'tie') {
-            computerMove = 'rock';
-            displayWinner = 'The round is a tie.';
-        }
-    }
-    else if (playerMove == 'paper') {
-        if (roundResult == 'playerWin') {
-            computerMove = 'rock';
-            displayWinner = 'You won the round!';
-        }
-        if (roundResult == 'computerWin') {
-            computerMove = 'scissors';
-            displayWinner = 'The computer won the round!';
-        }
-        if (roundResult == 'tie') {
-            computerMove = 'paper';
-            displayWinner = 'The round is a tie.';
-        }
-    }
-    if (playerMove == 'scissors') {
-        if (roundResult == 'playerWin') {
-            computerMove = 'paper';
-            displayWinner = 'You won the round!';
-        }
-        if (roundResult == 'computerWin') {
-            computerMove = 'rock';
-            displayWinner = 'The computer won the round!';
-        }
-        if (roundResult == 'tie') {
-            computerMove = 'scissors';
-            displayWinner = 'The round is a tie.';
-        }
-    }
-    if (playerScore > computerScore) {
-        displayScore = "You're in the lead, " + playerScore + " to " + computerScore + ".";
-    }
-    if (computerScore > playerScore) {
-        displayScore = "The computer is in the lead, " + computerScore + " to " + playerScore + ".";
-    }
-    if (playerScore == computerScore) {
-        displayScore = "The game is tied, " + playerScore + " to " + computerScore + ".";
-    }
-    li.innerText = 'You played ' + playerMove + ' and the computer played ' + computerMove + '. ' + displayWinner + " " + displayScore;
-    roundList.appendChild(li);
-}
-
 //Adds one point to the winner of the round.
 function addScore(score) {
+    console.log(score);
     score++;
+    console.log(score);
     return score;
 }
 
-//Displays the final winner and score.
-function endGame(playerScore, computerScore) {
-    const gameResult = document.getElementById('gameResult');
-    console.log(gameResult, playerScore, computerScore);
-    if (playerScore > computerScore) {
-        gameResult.textContent = "You won the game, " + playerScore + " to " + computerScore + "!";
-    }
-    if (playerScore < computerScore) {
-        gameResult.textContent = "You lost the game, " + computerScore + " to " + playerScore + "!";
-    }
+function updateDisplay() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 }
